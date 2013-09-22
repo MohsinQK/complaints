fc6afb46327acc0c38411992488e2a26e7f12098
Mohsin Khan <mohsin@cassini.gosign.de>
2013-09-22 11:25:52 +0200

    configuration added

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+  define('DB_ADAPTER', 'mysql');
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'username');
+  define('DB_PASS', 'password');
+  define('DB_NAME', 'database');
+  define('DB_PERSIST', true);
+  define('TABLE_PREFIX', 'fo_');
+  define('DB_ENGINE', 'InnoDB');
+  define('ROOT_URL', 'baseurl');
+  define('DEFAULT_LOCALIZATION', 'en_us');
+  define('COOKIE_PATH', '/');
+  define('DEBUG', false);
+  define('SEED', '68f6ff7161ddb7702c49a1ae5e4404a3');
+  define('DB_CHARSET', 'utf8');
+  return true;
+?>
